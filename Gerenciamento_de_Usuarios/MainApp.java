9376f57c4d5efd96ed421f7df8b092828c9bb9de
jefferson felix <jeffersson.jaily@gmail.com>
2025-09-05 02:13:14 -0300

    executavel

--- Gerenciamento_de_Usuarios/MainApp.java
+++ Gerenciamento_de_Usuarios/MainApp.java
@@ -1,22 +1,23 @@
 
-    import javafx.fxml.FXML;
-    import javafx.application.Application;
-    import javafx.fxml.FXMLLoader;
-    import javafx.scene.Parent;
-    import javafx.scene.Scene;
-    import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public class MainApp extends Application {
+public class MainApp extends Application {
 
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            Parent root = FXMLLoader.load(getClass().getResource("UsuarioView.fxml"));
-            primaryStage.setTitle("Sistema de Gerenciamento de Usuários");
-            primaryStage.setScene(new Scene(root, 700, 400));
-            primaryStage.show();
-        }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/UsuarioView.fxml"));
 
-        public static void main(String[] args) {
-            launch(args);
-        }
+        primaryStage.setTitle("Sistema de Gerenciamento de Usuários");
+        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
